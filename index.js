b6b840959ac1fdb8d1828433a91443ad57ea4634
Mayank <mayankkandk@gmail.com>
2025-07-15 10:56:04 +0530

    Add function to check for duplicate numbers in an array

--- index.js
+++ index.js
@@ -2166,3 +2166,15 @@ var isIsomorphic = function (s, t) {
   }
   return true;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  const sortedNums = nums.sort((a, b) => a - b);
+  for (let i = 0; i < sortedNums.length - 1; i++) {
+    if (sortedNums[i] === sortedNums[i + 1]) return false;
+  }
+  return true;
+};
